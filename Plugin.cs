c4966abeace528a1c6490498662eaa1e8bde716c
Gregory Brutsche <gregory@mail.brutsches.local>
2021-10-13 15:47:05 -0400

    Final touches for the workshop update.

--- Plugin.cs
+++ Plugin.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 namespace Smg_Goes_Brrrr {
     [UsedImplicitly]
     public class Plugin : BaseGameMod {
-        protected override     string ModName => "SMG-Goes-Brrrr";
         public static readonly GUID   SMG      = GUID.Parse("96048d612ab1ea246b50745a12b9b05e");
         private const          string NEW_NAME = "It Goes Brrrrrrrrrr";
         private const          string NEW_DESC = "Brrrrrrrrrrrrrrrrrrrrrrrr";
 
         public override void OnInitData() {
-            var smg = GameResources.Instance.Items.FirstOrDefault(item => item.AssetId == SMG);
+            var smg = RuntimeAssetDatabase.Get<ItemDefinition>().FirstOrDefault(item => item.AssetId == SMG);
             if (smg is null) {
                 Debug.Log("Error: Cannot find SMG item.");
                 return;
