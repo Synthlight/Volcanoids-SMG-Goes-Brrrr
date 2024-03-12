b9aa8c6e49fde92a01981fd1723dca51d97f787f
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-12 17:36:43 -0500

    Update for Volc 1.31.

--- Plugin.cs
+++ Plugin.cs
@@ -3,25 +3,25 @@ using Base_Mod;
 using JetBrains.Annotations;
 using UnityEngine;
 
-namespace Smg_Goes_Brrrr {
-    [UsedImplicitly]
-    public class Plugin : BaseGameMod {
-        public static readonly GUID   SMG      = GUID.Parse("96048d612ab1ea246b50745a12b9b05e");
-        private const          string NEW_NAME = "It Goes Brrrrrrrrrr";
-        private const          string NEW_DESC = "Brrrrrrrrrrrrrrrrrrrrrrrr";
+namespace Smg_Goes_Brrrr;
 
-        public override void OnInitData() {
-            var smg = RuntimeAssetDatabase.Get<ItemDefinition>().FirstOrDefault(item => item.AssetId == SMG);
-            if (smg is null) {
-                Debug.Log("Error: Cannot find SMG item.");
-                return;
-            }
+[UsedImplicitly]
+public class Plugin : BaseGameMod {
+    public static readonly GUID   SMG      = GUID.Parse("96048d612ab1ea246b50745a12b9b05e");
+    private const          string NEW_NAME = "It Goes Brrrrrrrrrr";
+    private const          string NEW_DESC = "Brrrrrrrrrrrrrrrrrrrrrrrr";
 
-            smg.NameLocalization        = new LocalizedString(".", NEW_NAME, ".");
-            smg.DescriptionLocalization = new LocalizedString(".", NEW_DESC, ".");
-            LocalizationManager.Localize(smg);
-
-            base.OnInitData();
+    public override void OnInitData() {
+        var smg = RuntimeAssetDatabase.Get<ItemDefinition>().FirstOrDefault(item => item.AssetId == SMG);
+        if (smg is null) {
+            Debug.Log("Error: Cannot find SMG item.");
+            return;
         }
+
+        smg.NameLocalization        = new(".", NEW_NAME, ".");
+        smg.DescriptionLocalization = new(".", NEW_DESC, ".");
+        LocalizationManager.Localize(smg);
+
+        base.OnInitData();
     }
 }
